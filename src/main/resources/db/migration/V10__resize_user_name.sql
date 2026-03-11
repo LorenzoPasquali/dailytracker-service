190e02ba15a6e277961e0dd6abe1d1eb67a675a9
Lorenzo <lorenzoapasquali@gmail.com>
2026-03-10 21:31:46 -0300

    fix: migration error userName varchar

--- src/main/resources/db/migration/V10__resize_user_name.sql
+++ src/main/resources/db/migration/V10__resize_user_name.sql
@@ -1,4 +1,4 @@
-ALTER TABLE "User" ALTER COLUMN "name" TYPE VARCHAR(20);
-
 -- Truncate any existing names that exceed the new limit
 UPDATE "User" SET "name" = LEFT("name", 20) WHERE LENGTH("name") > 20;
+
+ALTER TABLE "User" ALTER COLUMN "name" TYPE VARCHAR(20);
